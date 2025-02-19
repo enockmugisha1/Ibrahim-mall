10713c6100e1447369b46a929d1a38ee38411db9
Burabyo <j.burabyo@alustudent.com>
2025-02-19 14:36:31 +0200

    Co-authored-by: Ibrahim-Iradukunda <Ibrahim-Iradukunda@users.noreply.github.com> Co-authored-by: Larissa4-droid <Larissa4-droid@users.noreply.github.com> Co-authored-by: N-SilverJr <N-SilverJr@users.noreply.github.com> Co-authored-by: Divine Ebube Ifechukwude <difebi14@gmail.com>

--- ibra-mall/src/app/page.tsx
+++ ibra-mall/src/app/page.tsx
@@ -18,93 +18,103 @@ export default function Home() {
           <h1 className="text-center font-bold text-[30px] text-pink-500">
             WELCOME TO<span className="text-pink-800"> IbraMall</span>
           </h1>
-          <h2 className="text-[20px] mt-[20px]">
+          <h1 className="text-[20px] mt-[20px]">
             Explore a Wide Range of Premium Products at IbraMall From
             Cutting-Edge Technology to Stylish Fashion and Essential Home
             Appliances, We’ve Got Everything You Need to Live Better, Smarter,
             and More Stylishly
-          </h2>
+          </h1>
         </section>
         <section className="bg-pink-50">
-          <h1 className="text-center p-5 font-bold text-">
+         <div className=" p-5"> <h2 className="text-center p-5 font-bold text-[20px]">
             Smartphones and Accessories
-          </h1>
-          <div className="grid-col-8  justify-between bg-pink-200">
-            <span className="col-span-1 border-2 border-pink-500 p-5">
+          </h2> </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 bg-pink-200  gap-5">
+            <div className="border-2 border-pink-500 p-5">
               <h2 className="font-bold ">Smartphones:</h2>{" "}
               <p>
                 Latest models featuring touchscreen technology, designed for
                 enhanced performance and user experience.
               </p>
-            </span>
-            <span className="col-span-1 grid border-2 border-pink-500 p-5">
+            </div>
+            
+            <div className="grid border-2 border-pink-500 p-5">
               <h2 className="font-bold ">Cellphones:</h2>{" "}
               <p>
                 Reliable and durable phones that meet everyday communication
                 needs, perfect for all users.
               </p>
-            </span>
-            <span className="col-span-1 grid border-2 border-pink-500 p-5">
+            </div>
+            <div className="grid border-2 border-pink-500 p-5">
               <h2 className="font-bold ">Battery:</h2>{" "}
               <p>
                 High-capacity batteries for longer usage and minimal downtime.
               </p>
-            </span>
-            <span className="col-span-1 grid border-2 border-pink-500 p-5">
+            </div>
+            <div className="grid border-2 border-pink-500 p-5">
               <h2 className="font-bold">Charger:</h2>{" "}
               <p>
                 Fast-charging solutions to keep you powered up and ready to go.
               </p>
-            </span>
-            <span className="col-span-1 grid border-2 border-pink-500 p-5">
+            </div>
+            <div className="grid border-2 border-pink-500 p-5">
               <h2 className="font-bold ">Antiblock:</h2>{" "}
               <p>
                 Screen protectors or anti-glare films to keep your device safe
                 from scratches and smudges.
               </p>
-            </span>
-            <span className="col-span-1 grid border-2 border-pink-500 p-5">
+            </div>
+            <div className="grid border-2 border-pink-500 p-5">
               <h2 className="font-bold">Pouchette:</h2>{" "}
               <p>
                 Stylish and functional phone pouches for carrying your device
                 securely and conveniently.
               </p>
-            </span>
-            <span className="col-span-1 grid border-2 border-pink-500 p-5">
+            </div>
+            <div className="grid border-2 border-pink-500 p-5">
               <h2 className="font-bold">USB:</h2>{" "}
               <p>
                 High-speed USB cables and adapters for easy data transfer and
                 charging.
               </p>
-            </span>
-          </div>
+            </div>
+            </div>
         </section>
-        <section>
-          <h1>Fashion Apparel</h1>
-          <span>
-            Trendy Clothes: A wide range of stylish apparel for both men and
+        
+        <section className="bg-pink-50">
+          <h2 className="text-center p-5 font-bold text-[20px]">Fashion Apparel</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 bg-pink-200  gap-5">
+          <div className="grid border-2 border-pink-500 p-5">
+            <strong>Trendy Clothes:</strong> A wide range of stylish apparel for both men and
             women, featuring the latest fashion trends for casual and formal
             occasions.
-          </span>
-          <span>
-            Casual Wear: Comfortable yet fashionable pieces perfect for everyday
+          </div>
+          <div className="grid border-2 border-pink-500 p-5">
+            <strong>Casual Wear:</strong> Comfortable yet fashionable pieces perfect for everyday
             wear, including t-shirts, jeans, and sneakers.
-          </span>
-          <span>
-            Formal Outfits: Elegant dresses, suits, and blouses for professional
+          </div>
+          <div className="grid border-2 border-pink-500 p-5">
+            <strong>Formal Outfits:</strong> Elegant dresses, suits, and blouses for professional
             settings or special events.
-          </span>
-          <span>
-            Accessories: Complement your look with trendy accessories such as
+          </div>
+          <div className="grid border-2 border-pink-500 p-5">
+            <strong>Accessories:</strong> Complement your look with trendy accessories such as
             hats, scarves, watches, and jewelry to add a touch of
             sophistication.
-          </span>
-          <span>
-            Footwear: A variety of shoes, from casual sneakers to formal dress
+          </div>
+          <div className="grid border-2 border-pink-500 p-5">
+            <strong>Footwear:</strong> A variety of shoes, from casual sneakers to formal dress
             shoes, designed for both comfort and style.
-          </span>
+          </div>
+          </div>
         </section>
       </div>
+      <footer className="w-full text-center bg-gray-800 text-white p-3" >
+        <span>
+          <p>&copy; All Rights reserved</p>
+        </span>
+      </footer>
     </main>
   );
 }
+ 
